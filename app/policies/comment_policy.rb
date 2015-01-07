9f9f64edf638e1614ec1af40939af434b2d00c8d
tommybyrne <tbyrn687@gmail.com>
2015-01-06 21:12:49 -0600

    Added the ability to delete topics, posts, comments

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -3,4 +3,8 @@ class CommentPolicy <  ApplicationPolicy
   def create?
     user.present?
   end
+
+  def destroy?
+    create? && (record.user == user || user.admin? || user.moderator?)
+  end
 end
