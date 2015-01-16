8317bea68eb4c4989e6d29ad71f040661b6485a7
tommybyrne <tbyrn687@gmail.com>
2015-01-15 23:58:14 -0600

    Added js to be able to use ajax to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
 
 def create
   @post = Post.find( params[:post_id] )
-  @comments = @post.comments
-
   @comment = current_user.comments.build(comment_params)
   @comment.post = @post
-  @new_comment = Comment.new
   authorize @comment
+
+
 
   if @comment.save
     flash[:notice] = "Comment was Created Successfully."
@@ -21,15 +21,17 @@ end
 def destroy
   @post = Post.find(params[:post_id])
   @comment = @post.comments.find(params[:id])
-
   authorize @comment
+
   if @comment.destroy
     flash[:notice] = "Comment was removed."
-    redirect_to [@post.topic, @post]
   else
     flash[:error] = "Comment couldn't be deleted. Try again."
-    redirect_to [@post.topic, @post]
   end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
+    end
 end
 
 private
