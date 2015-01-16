4670b33c29b4697a76b870298d1beb4e9e3ead27
tommybyrne <tbyrn687@gmail.com>
2015-01-15 22:33:25 -0600

    Removed Test_factories module replaced with FactoryGirl syntax

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,9 +7,9 @@ describe Comment do
   describe "after_create" do
 
     before do
-      @post = associated_post
-      @user = authenticated_user
-      @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
+      @post = create(:post)
+      @user = create(:user)
+      @comment = create(:comment, user: @user, post: @post)
     end
 
     # We don't need to change anything for this condition:
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,35 +2,32 @@ require 'rails_helper'
 
 describe User do
 
-  include TestFactories
 
-   before do
-      @post = associated_post
-      @user = authenticated_user
-    end
+  before do
+    @user1 = create(:user)
+    post = create(:post, user: @user1)
+    create(:comment, user: @user1, post: post)
+
+    @user2 = create(:user)
+    post = create(:post, user: @user2)
+    2.times { create(:comment, user: @user2, post: post) }
+  end
+
 
   describe "#favorited(post)" do
     it "returns 'nil' if the user has not favorited the post" do
-      @user.favorites
+      @user1.favorites
+
     end
 
     it "returns the appropriate favorite if it exists" do
-      @user.favorites.where(post: @post).create
-      @user.favorites
+      @user1.favorites.where(post: @post).create
+      @user1.favorites
     end
   end
 
   describe ".top_rated" do
 
-    before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
-
-      @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post) }
-    end
 
     it "stores a 'posts_count' on user" do
       users = User.top_rated
